9beff4206a178759ab9ea59dbcb4da78a960684b
hbutt <hbutt@student.s19.be>
2024-04-11 22:26:32 +0200

    fin

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -10,4 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putnbr_fd(int n, int fd);
+#include "libft.h"
+
+void ft_putnbr_fd(int n, int fd)
+{
+    long int nbr;
+    unsigned char c;
+
+    nbr = n;
+    if (nbr < 0)
+    {
+        nbr = -nbr;
+        write(fd, "-", 1);
+    }
+    if (nbr >= 10)
+        ft_putnbr_fd(nbr / 10, fd);
+    c = (nbr % 10) + '0';
+    write(fd,&c, 1);
+}
--- libft.h
+++ libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 /* Partie 1 : libc */
 int		ft_isalpha(int c);
@@ -24,7 +25,7 @@ int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
-void	*ftmemcpy(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, char *src, size_t size);
