98d61ace28bff405090b845596386bc6a64e7e75
Hasnain Butt <hbutt@shi-r1-p4.s19.be>
2024-04-13 15:27:59 +0200

    ok

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:27:22 by hbutt             #+#    #+#             */
-/*   Updated: 2024/04/13 14:33:14 by hbutt            ###   ########.fr       */
+/*   Updated: 2024/04/13 15:08:31 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,11 @@
 
 void ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	
+	if (!lst || !(*lst) || !del)
+		return ;
+	if ((*lst)->next)
+		ft_lstclear((&(*lst)->next), del);
+	del((*lst)->content);
+	free(*lst);
+	*lst = NULL;
 }
--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -1,15 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:28:49 by hbutt             #+#    #+#             */
-/*   Updated: 2024/04/12 16:29:02 by hbutt            ###   ########.fr       */
+/*   Updated: 2024/04/13 15:10:05 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstiter(t_list *lst, void (*f)(void *));
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while(lst)
+	{
+		f(lst->content);
+		lst  = lst->next;
+	}
+}
